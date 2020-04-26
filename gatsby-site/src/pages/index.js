d58454630ba380b1463177f246b84fc646d87fe5
hamishakl <hamish.tana24@gmail.com>
2020-04-26 18:54:44 +1200

    changed gatsby index page to a class component and added in giphy api.

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,11 +1,11 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+class IndexPage extends Component {
 
   constructor(props) {
     super(props)
@@ -15,20 +15,35 @@ const IndexPage = () => (
     }
   }
 
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <ul>
-    <li><Link to="/page-2/">Go to page 2</Link></li>
-    <li><Link to="/page-3/">Go to page 3</Link></li>
-    <li><Link to="/page-4/">Go to page 4</Link></li>
-    </ul>
-  </Layout>
-)
+  componentDidMount() {
+    fetch("http://api.giphy.com/v1/gifs/trending")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <ul>
+        <li><Link to="/page-2/">Go to page 2</Link></li>
+        <li><Link to="/page-3/">Go to page 3</Link></li>
+        <li><Link to="/page-4/">Go to page 4</Link></li>
+        </ul>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
